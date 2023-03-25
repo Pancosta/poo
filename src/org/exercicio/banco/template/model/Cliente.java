0c8fcabe142d180d794c8d11cc320e56af72b66a
Pancosta <dz6.gustav@gmail.com>
2023-03-25 20:24:48 -0300

    correção de erro no teste + upando o repositório inteiro

--- src/org/exercicio/banco/template/model/Cliente.java
+++ src/org/exercicio/banco/template/model/Cliente.java
@@ -1,16 +1,16 @@
 package org.exercicio.banco.template.model;
-
 import java.util.ArrayList;
+import java.text.DecimalFormat;
 
 /**
-*
-* Nome:
-* Curso:
-* Matrícula:
-* 
-*/
+ *
+ * Nome: Gustavo Dantas
+ * Curso: TSI 2022.1
+ * Matrícula: 20201tsiig0134
+ * 
+ */
 public class Cliente {
-	
+
     private String nome;
     private String cpf;
     private ArrayList<ContaBancaria> contas;
@@ -22,59 +22,89 @@ public class Cliente {
     }
 
     /**
-     * O mehtodo recebe como argumento uma contabancaria a ser adicionada 
-     * ao arraylist de contas do cliente. Antes de adicionar, verifique se a 
-     * conta jah existe no array, caso jah exista a msg deve ser exibida no 
+     * O mehtodo recebe como argumento uma contabancaria a ser adicionada
+     * ao arraylist de contas do cliente. Antes de adicionar, verifique se a
+     * conta jah existe no array, caso jah exista a msg deve ser exibida no
      * console: "A conta jah estah associada a este cliente.". Caso contrahrio,
-     * imprima no console: "Conta adicionada com sucesso!". Utilize 
+     * imprima no console: "Conta adicionada com sucesso!". Utilize
      * System.out.print() para realizar as impressoes.
+     * 
      * @param c
      */
     public void adicionarConta(ContaBancaria c) {
 
+        if (this.contas.contains(c)) {
+            System.out.print("A conta jah estah associada a este cliente.");
+        } else {
+            this.contas.add(c);
+            System.out.print("Conta adicionada com sucesso!");
+        }
     }
 
-    
     /**
-     * O mehtodo recebe como argumento uma contabancaria a ser removida 
-     * do arraylist de contas do cliente. Antes de remover, verifique se a 
+     * O mehtodo recebe como argumento uma contabancaria a ser removida
+     * do arraylist de contas do cliente. Antes de remover, verifique se a
      * conta existe no array, caso o arraylist contenha a conta passada,
-     * a msg deve ser exibida no console: "Conta removida com sucesso!". 
-     * Caso contrahrio, imprima no console: "A conta nao esta associada a este cliente.".
+     * a msg deve ser exibida no console: "Conta removida com sucesso!".
+     * Caso contrahrio, imprima no console: "A conta nao esta associada a este
+     * cliente.".
      * Utilize System.out.print() para realizar as impressoes.
+     * 
      * @param c
      */
     public void removerConta(ContaBancaria c) {
+        if (this.contas.contains(c)) {
+            this.contas.remove(c);
+            System.out.print("Conta removida com sucesso!");
+        } else {
+            System.out.print("A conta nao esta associada a este cliente.");
+        }
 
     }
 
     /**
-     * Mehtodo recebe como argumento o numero da conta que deve ser 
+     * Mehtodo recebe como argumento o numero da conta que deve ser
      * procurado na lista de contas, havendo ocorrehncia de elemento
      * com o mesmo nuhmero de conta, a msg "Conta encontrada!" deve
      * ser impressa no console e logo apohs a conta deve ser retornada.
      * Nao havendo ocorrehncia, a msg "Conta nao encontrada." deve ser
-     * impressa no console e o retorno deve ser null.   
+     * impressa no console e o retorno deve ser null.
+     * 
      * @param numero
      * @return
      */
     public ContaBancaria localizarContaNumero(int numero) {
+        for (int i = 0; i < this.contas.size(); i++) {
+            if (this.contas.get(i).getNumeroConta() == numero) {
+                System.out.println("Conta encontrada!");
+                return this.contas.get(i);
+            } else {
+                System.out.print("Conta nao encontrada.");
+            }
+        }
         return null;
+
     }
 
-    
     /**
-     * Mehtodo recebe como argumento o objeto contabancaria que deve ser 
+     * Mehtodo recebe como argumento o objeto contabancaria que deve ser
      * procurado na lista de contas, havendo ocorrehncia de elemento
      * com no arraylist, a msg "Conta encontrada!" deve
-     * ser impressa no console e logo apohs o valor booleano true deve ser retornado.
+     * ser impressa no console e logo apohs o valor booleano true deve ser
+     * retornado.
      * Nao havendo ocorrehncia, a msg "Conta nao encontrada." deve ser
-     * impressa no console e o retorno deve ser false.   
+     * impressa no console e o retorno deve ser false.
+     * 
      * @param numero
      * @return
      */
     public boolean localizarConta(ContaBancaria c) {
-        return false;
+        if (this.contas.contains(c)) {
+            System.out.println("Conta encontrada!");
+        } else {
+            System.out.print("Conta nao encontrada.");
+        }
+        return this.contas.contains(c);
     }
 
     /**
@@ -83,21 +113,28 @@ public class Cliente {
      * contas, o saldo total deve ser impresso por meio da msg no
      * console: "Balanco entre contas: RS", concatenado ao saldo total.
      * O valor tambehm deve ser retornado, logo apohs a impressao.
+     * 
      * @return
      */
     public double balancoEntreContas() {
-        return 0.0;
+        double saldo = 0;
+
+        for (int i = 0; i < this.contas.size(); i++) {
+            saldo += this.contas.get(i).getSaldo();
+        }
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.print("Balanco entre contas: RS" + df.format(saldo));
+        return saldo;
     }
-    
+
     public ArrayList<ContaBancaria> getContas() {
-		return contas;
-	}
-    
+        return contas;
+    }
+
     public void setContas(ArrayList<ContaBancaria> contas) {
-		this.contas = contas;
-	}
-      
-    // getters e setters omitidos
-    
+        this.contas = contas;
+    }
+
 }
 
+// getters e setters omitidos
